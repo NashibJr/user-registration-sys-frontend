e36c7bf5f32dd3bdde6fbe99dfb6041cf85a58e0
Nashib Jr <nashibjr@gmail.com>
2024-04-29 11:36:26 -0700

    Feat: Added the signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,32 +2,29 @@ import Button from "@/components/Button";
 import Input from "@/components/Input";
 import PageComponent from "@/components/PageComponent";
 import Link from "next/link";
+import React from "react";
 
-const Login = () => {
+const SignUp = () => {
   return (
     <PageComponent
-      title="Welcomeback"
-      paragraph="Dear user provide your correct credentials and signin. If you dont
-    have an account feel free to create on by clicking in the signup
-    link"
+      title="Create a free account"
+      paragraph="Dear user, create your account by providing your username, email and password."
     >
       <form className="lg:pl-12 lg:pr-12 flex flex-col gap-4 w-full sm:w-[400px] md:w-full mt-20 md:mt-auto mx-auto">
         <h2 className="font-bold text-white text-2xl md:text-3xl text-center opacity-80">
-          Sign in
+          Sign up
         </h2>
         <Input label="Username" placeholder="Username" />
+        <Input label="Email" placeholder="Email" />
         <Input label="Password" placeholder="Password" />
         <Button
           label="Sign in now"
           className="text-white outline-none p-2 rounded-lg bg-orange-600 hover:opacity-60 opacity-70 w-full lg:w-[200px]"
         />
-        <Link href="#" className="text-white">
-          Lost your password?
-        </Link>
-        <p className="text-white -mt-4">
-          Don't have an account?{" "}
-          <Link href="/signup" className="text-[dodgerblue] hover:underline">
-            Sign up
+        <p className="text-white">
+          Already have an account?{" "}
+          <Link href="/" className="text-[dodgerblue] hover:underline">
+            Sign in
           </Link>
         </p>
       </form>
@@ -35,4 +32,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
